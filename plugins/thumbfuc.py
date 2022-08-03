c6a16867d3970900a93dc7d2508a09417075fb7e
⭐star⭐ <107930379+starneww@users.noreply.github.com>
2022-08-03 20:15:05 +0530

    Update thumbfuc.py

--- plugins/thumbfuc.py
+++ plugins/thumbfuc.py
@@ -9,12 +9,12 @@ async def viewthumb(client,message):
 	   chat_id=message.chat.id, 
 	   photo=thumb)
     else:
-        await message.reply_text("**💋 ʙᴀʙʏ Yᴏᴜ ᴅᴏɴᴛ ʜᴀᴠᴇ ᴀɴʏ Tʜᴜᴍʙɴᴀɪʟ**❌") 
+        await message.reply_text("**💋 ʙᴀʙʏ Yᴏᴜ ᴅᴏɴᴛ ʜᴀᴠᴇ ᴀɴʏ Tʜᴜᴍʙɴᴀɪʟ** ❌") 
 		
 @Client.on_message(filters.private & filters.command(['delthumb']))
 async def removethumb(client,message):
     delthumb(int(message.chat.id))
-    await message.reply_text("**💋 ʙᴀʙʏ ʏᴏᴜʀ Cᴜsᴛᴏᴍ Tʜᴜᴍʙɴᴀɪʟ Dᴇʟᴇᴛᴇᴅ Sᴜᴄᴄᴇssғᴜʟʟʏ**🎉")
+    await message.reply_text("**💋 ʙᴀʙʏ ʏᴏᴜʀ Cᴜsᴛᴏᴍ Tʜᴜᴍʙɴᴀɪʟ Dᴇʟᴇᴛᴇᴅ Sᴜᴄᴄᴇssғᴜʟʟʏ** 🎉")
 	
 @Client.on_message(filters.private & filters.photo)
 async def addthumbs(client,message):
