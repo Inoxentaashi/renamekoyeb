1d0a17f34dab88a75a507c2d61587f3984af5c32
⭐star⭐ <107930379+starneww@users.noreply.github.com>
2022-08-03 19:58:08 +0530

    Update txt.py

--- helper/txt.py
+++ helper/txt.py
@@ -25,33 +25,33 @@ License Link : https://github.com/TEAM-PYRO-BOTZ/PYRO-RENAME-BOT/blob/main/LICEN
 
 class mr(object):
     PROGRESS_BAR = """\n
-╭━━━━❰ᴘʀᴏɢʀᴇss ʙᴀʀ❱━➣
-┣⪼ 🗂️ : {1} | {2}
-┣⪼ ⏳️ : {0}%
-┣⪼ 🚀 : {3}/s
-┣⪼ ⏱️ : {4}
-╰━━━━━━━━━━━━━━━➣ """
+╭━━━━❰✧ᴘʀᴏɢʀᴇSS ʙᴀʀ✧❱━➣
+┣⪼Fɪʟᴇ sɪᴢᴇ 🗂️ : {1} | {2}
+┣⪼Dᴜʀᴀᴛɪᴏɴ ⏳️ : {0}%
+┣⪼Sᴘᴇᴇᴅ 🚀 : {3}/s
+┣⪼Tɪᴍᴇ ʟᴇғᴛ ⏱️ : {4}
+╰━━━━━━━━━━━━━━━✧✧➣ """
 
     ABOUT_TXT = """
-╭───────────⍟
-├🤖 ᴍʏ ɴᴀᴍᴇ : {}
+╭───────────✧✧✧✧✧✧⍟
+├🤖 ᴍʏ ɴᴀᴍᴇ : <b>{}</b>
 ├🔮 ᴏᴛʜᴇʀ ʙᴏᴛS : <a href=https://t.me/robo_glitch>〘 **Cʟɪᴄᴋ ʜᴇʀᴇ** 〙</a> 
 ├👨‍💻 Dᴇᴠᴇʟᴏᴘᴇʀ : <a href=https://t.me/the_glitchs>〘 **Cʟɪᴄᴋ ʜᴇʀᴇ** 〙</a>
 ├🍂 Sᴜᴘᴘᴏʀᴛ : <a href=https://t.me/hddubhub4uhelp>〘 **Cʟɪᴄᴋ ʜᴇʀᴇ** 〙</a>
-├📽 Mᴏᴠɪᴇs ɢʀᴏᴜᴘ : <a href=https://t.me/dubbedweb>〘 **Cʟɪᴄᴋ ʜᴇʀᴇ** 〙</a>
-├📢 Mᴀɪɴ ᴄʜᴀɴɴᴇʟ : <a href=https://t.me/hddubhub4u>〘 **Cʟɪᴄᴋ ʜᴇʀᴇ** 〙</a>
-├📈 Bᴏᴛs ʟɪᴠᴇ Sᴛᴀᴛᴜs : <a href=https://t.me/https://t.me/futurebackups/754>〘 **Cʟɪᴄᴋ ʜᴇʀᴇ** 〙</a>
-├📊 Pʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ : v3.6.8 [ ᗷᗴTᗩ ]              
-╰───────────────⍟
+├📽 Mᴏᴠɪᴇɢʀᴏᴜᴘ : <a href=https://t.me/dubbedweb>〘 **Cʟɪᴄᴋ ʜᴇʀᴇ** 〙</a>
+├📢 ᴄʜᴀɴɴᴇʟ : <a href=https://t.me/hddubhub4u>〘 **Cʟɪᴄᴋ ʜᴇʀᴇ** 〙</a>
+├📈 Bᴏᴛs Sᴛᴀᴛᴜs : <a href=https://t.me/https://t.me/futurebackups/754>〘 **Cʟɪᴄᴋ ʜᴇʀᴇ** 〙</a>
+├📊 Pʏʀ ᴠᴇʀsɪᴏɴ : v3.6.8 [ ᗷᗴTᗩ ]              
+╰──────────────✧✧✧✧⍟
                                 """
     HELP_TXT = """
-🌌 <b><u>HOW TO SET THUMBNILE</u></b>
+🌌 <b><u>✧HOW TO SET THUMBNILE✧</u></b>
   
 •> /start a bot and send any picture to automatically set thumbnile.
 •> /delthumb use this command and delete your old thumbnile.
 •> /viewthumb use this command view your current thumbnile.
 
-📑 <b><u>HOW TO SET CUSTOM CAPTION</u></b>
+📑 <b><u>✧HOW TO SET CUSTOM CAPTION✧</u></b>
 •> /set_caption - set a custom caption
 •> /see_caption - see your custom caption
 •> /del_caption - delete custom caption
@@ -60,16 +60,16 @@ Example:- /set_caption 📕 File Name: {filename}
 💾 Size: {filesize}
 ⏰ Duration: {duration}
 
-✏️ <b><u>HOW TO RENAME A FILE</u></b>
+✏️ <b><u>✧HOW TO RENAME A FILE✧</u></b>
 •> send any file and click rename option and type new file name and \n send select [ document, video, audio ] 
 💌 ɴᴇᴇᴅ ᴀɴʏ ᴏᴛʜᴇʀ ʜᴇʟᴘ ᴄᴏɴᴛᴀᴄᴛ :- <a href=https://t.me/hddubhub4uhelp>🍂 Sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ 👥</a>
 """
 
-#⚠️ don't remove our credits 🙏😢😢
+#⚠️ don't remove our credits 
     DEV_TXT = """
-<b><u>🎭 Lɪᴛᴛʟᴇ ᴅᴇsᴄʀɪᴘᴛɪᴏɴ 🎭 </b></u> 
+<b><u>🎭 Lɪᴛᴛʟᴇ ᴅᴇSᴄʀɪᴘᴛɪᴏɴ 🎭 </b></u> 
 
-» 💌 ᴄᴏɴᴛᴀᴄᴛ : <a href=https://t.me/GlitchAssistantBot>**G̩̱ͩ̏͜ℓιтçђ ᴀssɪsᴛᴀɴᴛ❂**</a> | <a href=https://t.me/the_glitchs>༒**ᴛʜᴇ_ɢʟɪᴛᴄʜs**༒<a>
-» Hᴇʏ ʙᴜᴅᴅʏ ɪғ ʏᴏᴜ ᴡᴀɴᴛ ʏᴏᴜʀ ᴏᴡɴ ʙᴏᴛ ᴡɪᴛʜ ʏᴏᴜʀ ᴄʜᴀɴɴᴇʟ ɴᴀᴍᴇ & sᴄʀɪᴘᴛ ʟɪᴋᴇ ᴛʜɪs ᴏʀ ᴀɴʏ ᴏᴛʜᴇʀ ʙᴏᴛ ᴏʀ ʏᴏᴜ ɴᴇᴇᴅ ᴀɴʏ ʜᴇʟᴘ ʀᴇɢᴀʀᴅɪɴɢ ᴅᴇᴠᴇʟᴏᴘᴍᴇɴᴛ ᴏғ ᴘʏʀᴏɢʀᴀᴍ ʙᴏᴛs ᴏʀ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴅᴏɴᴀᴛᴇ ᴜs ғᴇᴇʟ ғʀᴇᴇ ᴛᴏ ᴄᴏɴᴛᴀᴄᴛ ᴠɪᴀ 
+➥ 💌 **ᴄᴏɴᴛᴀᴄᴛ** : <a href=https://t.me/GlitchAssistantBot>**『**via ᴀssɪsᴛᴀɴᴛ**』**</a> | <a href=https://t.me/the_glitchs>『**ᴛʜᴇ_ɢʟɪᴛᴄʜs**』</a>
+\n➥ Hᴇʏ ʙᴜᴅᴅʏ ɪғ ʏᴏᴜ ᴡᴀɴᴛ ʏᴏᴜʀ ᴏᴡɴ ʙᴏᴛ ᴡɪᴛʜ ʏᴏᴜʀ ᴄʜᴀɴɴᴇʟ ɴᴀᴍᴇ & Sᴄʀɪᴘᴛ ʟɪᴋᴇ ᴛʜɪS ᴏʀ ᴀɴʏ ᴏᴛʜᴇʀ ʙᴏᴛ ᴏʀ ʏᴏᴜ ɴᴇᴇᴅ ᴀɴʏ ʜᴇʟᴘ ʀᴇɢᴀʀᴅɪɴɢ ᴅᴇᴠᴇʟᴏᴘᴍᴇɴᴛ ᴏғ ᴘʏʀᴏɢʀᴀᴍ ʙᴏᴛs ᴏʀ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ᴅᴏɴᴀᴛᴇ ᴜs ғᴇᴇʟ ғʀᴇᴇ ᴛᴏ ᴄᴏɴᴛᴀᴄᴛ ᴠɪᴀ 
   Iғ ᴀssɪsᴛᴀɴᴛ ʙᴏᴛ ᴅᴇᴀᴅ ⚰ ᴛʜᴇɴ ᴍsɢ ᴏɴ ᴛʜᴇ ɢʟɪᴛᴄʜs ᴍᴇɴᴛɪᴏɴᴇᴅ ᴀʙᴏᴠᴇ 📍
   """
